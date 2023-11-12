85e629398d245c966c76aa18ba4fd7b89f4708a9
Ognjen <ognjenmilojevic2001@gmail.com>
2023-11-12 22:48:29 +0100

    feat: initial project structure

---
+++ MediShipping/src/test/java/com/isa/springboot/MediShipping/MediShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.springboot.MediShipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
