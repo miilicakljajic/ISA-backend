f3d392990152d8a255b33316105bcf2a02dd2e2c
Ognjen <ognjenmilojevic2001@gmail.com>
2024-01-29 18:10:41 +0100

    fix: implemented commandlinerunner

--- HospitalSimulator/src/main/java/com/isa/springboot/HospitalSimulator/HospitalSimulator.java
+++ HospitalSimulator/src/main/java/com/isa/springboot/HospitalSimulator/HospitalSimulator.java
@@ -1,12 +1,19 @@
 package com.isa.springboot.HospitalSimulator;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HospitalSimulator {
+public class HospitalSimulator implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(HospitalSimulator.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Disi");
     }
 }
