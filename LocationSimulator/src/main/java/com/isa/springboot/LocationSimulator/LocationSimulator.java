8e23a98b6d0f8b936c715a5a01534d1ec7adddcc
Ognjen <ognjenmilojevic2001@gmail.com>
2024-01-29 19:55:14 +0100

    feat: created initial location simulator

---
+++ LocationSimulator/src/main/java/com/isa/springboot/LocationSimulator/LocationSimulator.java
@@ -0,0 +1,19 @@
+package com.isa.springboot.LocationSimulator;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSimulator implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationSimulator.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Disi");
+    }
+}
