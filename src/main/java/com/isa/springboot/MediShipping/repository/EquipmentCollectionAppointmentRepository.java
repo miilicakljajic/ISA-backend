c9ce0e9ad6fca8024b5fda2c575f34fa39b6732a
Mihailo <zupermancile@gmail.com>
2024-01-30 22:13:21 +0100

    a

--- src/main/java/com/isa/springboot/MediShipping/repository/EquipmentCollectionAppointmentRepository.java
+++ src/main/java/com/isa/springboot/MediShipping/repository/EquipmentCollectionAppointmentRepository.java
@@ -5,14 +5,21 @@ import com.isa.springboot.MediShipping.dto.EquipmentCollectionAppointmentDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.LockModeType;
 import javax.persistence.QueryHint;
+import java.util.Optional;
 
 @Repository
 public interface EquipmentCollectionAppointmentRepository extends JpaRepository<EquipmentCollectionAppointment,Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout",value = "0")})
     public EquipmentCollectionAppointment save(EquipmentCollectionAppointment appointment);
+
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout",value = "0")})
+    public Optional<EquipmentCollectionAppointment> findById(@Param("id")Long id);
 }
